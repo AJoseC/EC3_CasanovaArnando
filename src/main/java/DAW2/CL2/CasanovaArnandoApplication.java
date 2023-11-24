15ef61589b1a5e03c8c46c5b038d49b15ba1e8fa
AJoseC <josecasanova.p1@gmail.com>
2023-11-23 20:56:54 -0500

    inicio

---
+++ src/main/java/DAW2/CL2/CasanovaArnandoApplication.java
@@ -0,0 +1,13 @@
+package DAW2.CL2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasanovaArnandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasanovaArnandoApplication.class, args);
+	}
+
+}
